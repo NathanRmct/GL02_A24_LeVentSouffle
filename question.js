1404f1192b58c6c3663b69472c7b1808eae9cd7d
Solène <138325816+solene-drnx@users.noreply.github.com>
2024-11-20 17:37:44 +0100

    attribut question

---
+++ question.js
@@ -0,0 +1,17 @@
+var question = function(type, title, sentence, answers, correctAnswers){
+	this.type = type; 
+	this.title = title;	
+	this.sentence = sentence; // String
+	this.answers = answers; // Liste de String
+	this.correctAnswers = correctAnswers // String
+}
+	
+/*
+question.prototype.averageRatings = function(){
+	var total = this.ratings.reduce((acc, elt) => acc + parseInt(elt), 0);
+	return total / this.ratings.length;
+
+};
+*/
+
+module.exports = question;
