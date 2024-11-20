4bceafde5a4d8ba0302d47f53bfe644e7059ba37
Arbalight <Heliotness45@gmail.com>
2024-11-21 00:04:58 +0100

    Ajout gitignore et modules
    Ajout du gitignore permettant de ne pas prendre en compte en modules.
    
    Attention : les modules colors, vega, vegalite et caporal sont importants pour bien faire fonctionner ce projet

---
+++ CaporalCli.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const colors = require('colors');
+const VpfParser = require('./GiftParser.js');
+const vg = require('vega');
+const vegalite = require('vega-lite');
+const cli = require("@caporal/core").default;
