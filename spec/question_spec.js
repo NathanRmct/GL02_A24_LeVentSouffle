d85acf61a8506394af2f4f8fea9d8e3977cd0ceb
Arbalight <Heliotness45@gmail.com>
2024-12-02 17:49:19 +0100

    Test unitaire : search dans une question

--- spec/question_spec.js
+++ spec/question_spec.js
@@ -4,7 +4,7 @@ describe("Program testing of question", function(){
 	
 	beforeAll(function() {
 
-		this.q = new question("Question test",["Description", "phrase 1", "phrase 2"], "type1",["Answer 1", "Answer 2"] , "Answer 2");
+		this.q = new question("Question test",["Description", "phrase 1", "phrase 2", "Answer 1", "Answer 2"], "type1",["Answer 1", "Answer 2"] , "Answer 2");
 
 	});
 	
@@ -13,10 +13,16 @@ describe("Program testing of question", function(){
 		expect(this.q).toBeDefined();
 		// toBe is === on simple values
 		expect(this.q.title).toBe("Question test");
-		expect(this.q).toEqual(jasmine.objectContaining({sentence: ["Description", "phrase 1", "phrase 2"]}));
+		expect(this.q).toEqual(jasmine.objectContaining({sentence: ["Description", "phrase 1", "phrase 2", "Answer 1", "Answer 2"]}));
 		expect(this.q.type).toBe("type1");
 		expect(this.q).toEqual(jasmine.objectContaining({answers: ["Answer 1", "Answer 2"]}));
 		expect(this.q.correctAnswers).toBe("Answer 2");
+	});
+
+	it("can create search in a question", function(){
+		search = "Descri";
+		expect(this.q.search(search)).toBeTrue();
+
 	});
 	
 	
